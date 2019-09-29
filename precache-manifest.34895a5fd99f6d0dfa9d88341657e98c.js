e6316dc851f161799aace0496700dbc0d7760e90
Amul Karuvath <akaruvath@highroads.com>
2019-09-29 19:40:01 +0530

    Updates

---
+++ precache-manifest.34895a5fd99f6d0dfa9d88341657e98c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8a4739c1faa25c4e2fd9b9501a95e3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b2ff9937ede02e82b756",
+    "url": "/monster-rolodex/static/css/main.db178bc0.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monster-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "b2ff9937ede02e82b756",
+    "url": "/monster-rolodex/static/js/main.6589aebd.chunk.js"
+  },
+  {
+    "revision": "b9d608cb1517ab0e40b5",
+    "url": "/monster-rolodex/static/js/runtime~main.8833da29.js"
+  }
+]);
